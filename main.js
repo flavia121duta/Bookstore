5dec3d819a71d3c8c10ec254968a36566fd5edc0
Flavia Duta <flaviaduta302@gmail.com>
2022-05-03 12:44:09 +0300

    a commit

--- main.js
+++
@@ -1,8 +0,0 @@
-function playSound(sound) {
-    let song = document.getElementById("sound");
-    song.volume = .25; // setting the volume to 25% because the sound is loud
-    if (song.paused)  // if song is paused
-        song.play();
-    else
-        song.pause();
-}
