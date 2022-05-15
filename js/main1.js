65d2e1a5d2aeba766afe04fbf2b14ab44dfb8e12
Flavia Duta <flaviaduta302@gmail.com>
2022-05-15 22:15:15 +0300

    a commit

--- js/main1.js
+++ js/main1.js
@@ -32,7 +32,7 @@ function changeBackground(){
 
 const list = document.querySelector('#notice .lista');
 
-list.addEventListener('click', (event) => {
+list.addEventListener('dblclick', (event) => {
     event.target.remove();
 });
 
@@ -42,7 +42,40 @@ const homeColors = ['pink', 'red', 'yellow', 'blue', 'white', 'violet', 'hotpink
 
 function coloreaza() {
     document.querySelector('#home').style.backgroundColor = homeColors[Math.floor(Math.random() * homeColors.length)];
+    document.querySelector('#history').style.backgroundColor = homeColors[Math.floor(Math.random() * homeColors.length)];
 }
 
+function delay() {
+    setTimeout(coloreaza, 1000);
+}
 document.querySelector('#home').addEventListener('mouseover', coloreaza);
+document.querySelector('#history').addEventListener('mouseover', delay);
+
+let container = document.getElementById("presentation");
+let t = setInterval(schimba,2000,container,"pink","yellow");
+function schimba(elem,culoare1,culoare2) {
+    if(elem.style.backgroundColor===culoare1)
+            elem.style.backgroundColor = culoare2;
+        else
+            elem.style.backgroundColor = culoare1;
+}
+
+container.onclick = function() {
+    clearInterval(t);
+}
+
+//----------------------------------------------------------
+
+let myWindow; // globala pt a fi vazuta si la apelul close()
+function openWin() {
+    myWindow = window.open("galerie.html", "_blank"); // TODO: nu deschide intr-o fereastra noua: C7, slide 38
+    myWindow.blur();
+}
+
+function closeWin() {
+    myWindow.close();
+}
+
+//----------------------------------------------------------
+// TODO: ceva cu stopPropagation
 
--- js/main2.js
+++ js/main2.js
@@ -1,24 +1,75 @@
 // 'use strict'
 
+window.onload = myMain;
+
+function myMain() {
+    document.querySelector('h1').onmouseover = stil1;
+    document.querySelector('h1').onmouseout = stil2;
+}
+
+function stil1() {
+    document.querySelector('h1').className = "stil1";
+}
+function stil2() {
+    document.querySelector('h1').className = "stil2";
+}
+
+//------------------------------------------------------
+
+// la alegerea unui element din lista, fundabul paginii se modifica in culoarea corespunzatoare
+
+function changeBG() {
+    let div = document.getElementById("BG");
+    let body = document.querySelector('body');
+
+    document.onload = function(event) {
+        switch (event.key) {
+            case "red":
+                body.style.color = "red";
+                break;
+            case "yellow":
+                body.style.color = "green";
+                break;
+            case "blue":
+                body.style.color = "blue";
+                break;
+            case "green":
+                body.style.color = "green";
+                break;
+            default:
+                console.log("Error");
+                return;
+        }
+        document.getElementById("tasta").innerHTML = event.key;
+    }
+}
+//------------------------------------------------------
+
 let figures = document.querySelectorAll('figure');
 console.log(figures);
 
-figures[0].pret = 32;
-figures[1].pret = 48;
-figures[2].pret = 70;
-figures[3].pret = 25;
-figures[4].pret = 30;
-figures[5].pret = 40;
-figures[6].pret = 20;
-figures[7].pret = 100;
-
-function compare(a, b) {
-    if (a.innerText > b.innerText)
-        return -1;
-    if (a.innerText < b.innerText)
-        return 1;
-    return 0;
+for (let img of figures) {
+    let info = String(img.innerHTML);
+    console.log(info.split('>'));
+    // TODO: folisind regex sau orice altceva pe lume, scoate tilul, pretul si sirsa imaginii
 }
+
+
+function Book(sursaImg, titlu, pret) {
+    this.sursaImg = sursaImg;
+    this.title = titlu;
+    this.pret = pret;
+}
+// let myCar = new Book("Volkswagen", "Golf 6", 140);
+
+
+// function compare(a, b) {
+//     if (a.innerText > b.innerText)
+//         return -1;
+//     if (a.innerText < b.innerText)
+//         return 1;
+//     return 0;
+// }
 
 let buton1
 
